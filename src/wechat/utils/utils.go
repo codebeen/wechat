e1e88a9706e44492aba7c20d3e31ef5b3896099c
zhangxiaoyong <zhangxiaoyong@intra.nsfocus.com>
2019-05-23 17:45:57 +0800

    Local adaptation

---
+++ src/wechat/utils/utils.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"path/filepath"
+	"os"
+	"strings"
+	"log"
+)
+
+func GetCurrentDirectory() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))  //返回绝对路径  filepath.Dir(os.Args[0])去除最后一个元素的路径
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Replace(dir, "\\", "/", -1) //将\替换成/
+}
+
